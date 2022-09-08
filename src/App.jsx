04fc9359f448bbb0ee6728d7e0d2e3cbd04a9644
thomaskokholm <thomas@kokholm.org>
2022-09-08 08:31:17 +0200

    2 files modified, 1 added, 2 deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  const renderImgOnCanvasById = (canvasId, imgSrc, idx) => {
+    const img = new Image();
+    const canvas = document.getElementById(canvasId);
+    if (canvas.getContext) {
+      const ctx = canvas.getContext('2d');
+      img.onload = function () {
+        let sx = parseInt((this.width / 3) * idx);
+        let sy = 0;
+        let sWidth = this.width / 3;
+        let sHeight = this.height;
+        let dx = 0;
+        let dy = 0;
+        let dWidth = this.width;
+        let dHeight = this.height;
+
+        ctx.drawImage(img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight); // Or at whatever offset you like
+
+        document.getElementById('img_' + idx).src = canvas
+          .toDataURL('image/png')
+          .replace('image/png', 'image/octet-stream');
+      };
+
+      img.src = imgSrc;
+    }
+  };
+  const onImageInput = (e) => {
+    e.preventDefault();
+
+    const imgSrc = URL.createObjectURL(e.target.files[0]);
+
+    let w = e.target;
+    renderImgOnCanvasById('canvasA', imgSrc, 0);
+    renderImgOnCanvasById('canvasB', imgSrc, 1);
+    renderImgOnCanvasById('canvasC', imgSrc, 2);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1>Triptych generator</h1>
+      <input type="file" accept="image/*" onChange={onImageInput} />
+      <div className="images">
+        <img id="img_0" />
+        <img id="img_1" />
+        <img id="img_2" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <canvas
+        id="canvasA"
+        width="5760"
+        height="3840"
+        className="canvas"
+      ></canvas>
+      <canvas
+        id="canvasB"
+        width="5760"
+        height="3840"
+        className="canvas"
+      ></canvas>
+      <canvas
+        id="canvasC"
+        width="5760"
+        height="3840"
+        className="canvas"
+      ></canvas>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
